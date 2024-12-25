4e8ac1e73289c265e294f9f597a0a4c27d3677f9
yakhousam <42283612+yakhousam@users.noreply.github.com>
2024-12-25 20:03:41 +0100

    Add validateUser middleware for user authentication

---
+++ src/custom-middlewares/validateUser.ts
@@ -0,0 +1,24 @@
+import middy from '@middy/core'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import createError from 'http-errors'
+
+const validateUserMiddleware = (): middy.MiddlewareObj<
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult
+> => {
+  return {
+    before: async (request): Promise<void> => {
+      const userId = process.env.userId
+        ? process.env.userId
+        : request.event.requestContext.authorizer?.claims.sub
+      if (userId === undefined) {
+        throw new createError.Unauthorized()
+      }
+      if (!request.event.requestContext.authorizer?.claims.sub) {
+        request.event.requestContext.authorizer = { claims: { sub: userId } } // it means  that userId is coming from the environment variable (local development)
+      }
+    }
+  }
+}
+
+export default validateUserMiddleware
