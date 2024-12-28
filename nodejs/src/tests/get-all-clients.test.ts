0d39b0a9eb7b3ca00be16f24c88bb42beb44a48a
yakhousam <42283612+yakhousam@users.noreply.github.com>
2024-12-28 18:16:47 +0100

    Refactor getAllClients test to use updated handler and improve error handling

--- nodejs/src/tests/get-all-clients.test.ts
+++ nodejs/src/tests/get-all-clients.test.ts
@@ -1,12 +1,32 @@
 import { DynamoDBDocumentClient, QueryCommand } from '@aws-sdk/lib-dynamodb'
-import { type APIGatewayProxyEvent } from 'aws-lambda'
+import { Context, type APIGatewayProxyEvent } from 'aws-lambda'
 import { mockClient } from 'aws-sdk-client-mock'
 import { beforeEach, describe, expect, it } from 'vitest'
-import { getClientsController } from '../controllers/getAllClients'
+import { handler as getAllClientsHandler } from '../../functions/getAllClients'
 import { generateClients } from './generate'
 
 describe('Test getAllClients', () => {
   const ddbMock = mockClient(DynamoDBDocumentClient)
+
+  const event = {
+    httpMethod: 'GET',
+    headers: {
+      'cache-control': 'no-cache'
+    },
+    requestContext: {
+      authorizer: {
+        jwt: {
+          claims: {
+            sub: 'userId'
+          }
+        }
+      }
+    }
+  } as unknown as APIGatewayProxyEvent
+
+  const context = {
+    getRemainingTimeInMillis: false
+  } as unknown as Context
 
   beforeEach(() => {
     ddbMock.reset()
@@ -19,115 +39,19 @@ describe('Test getAllClients', () => {
       Items: clients
     })
 
-    const event = {
-      httpMethod: 'GET',
-      requestContext: {
-        authorizer: {
-          claims: {
-            sub: 'userId'
-          }
-        }
-      }
-    } as unknown as APIGatewayProxyEvent
+    const result = await getAllClientsHandler(event, context)
 
-    const result = await getClientsController(event)
-
-    const expectedResult = {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ clients, lastEvaluatedKey: undefined })
-    }
-
-    expect(result).toEqual(expectedResult)
+    expect(result.statusCode).toBe(200)
+    expect(result.body).toEqual(
+      JSON.stringify({ clients, count: clients.length })
+    )
   })
 
   it('should return 500 on DynamoDB error', async () => {
     ddbMock.on(QueryCommand).rejects(new Error('DynamoDB error'))
 
-    const event = {
-      httpMethod: 'GET',
-      requestContext: {
-        authorizer: {
-          claims: {
-            sub: 'userId'
-          }
-        }
-      }
-    } as unknown as APIGatewayProxyEvent
+    const result = await getAllClientsHandler(event, context)
 
-    const result = await getClientsController(event)
-
-    const expectedResult = {
-      statusCode: 500,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(new Error('DynamoDB error'))
-    }
-
-    expect(result).toEqual(expectedResult)
-  })
-
-  it('should paginate clients', async () => {
-    const clients = generateClients(1)
-
-    ddbMock.on(QueryCommand).resolves({
-      Items: clients,
-      LastEvaluatedKey: { clientId: clients[0].clientId }
-    })
-
-    const event = {
-      httpMethod: 'GET',
-      queryStringParameters: {
-        lastEvaluatedKey: JSON.stringify({ clientId: 'clientId' })
-      },
-      requestContext: {
-        authorizer: {
-          claims: {
-            sub: 'userId'
-          }
-        }
-      }
-    } as unknown as APIGatewayProxyEvent
-
-    const result = await getClientsController(event)
-
-    const expectedResult = {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        clients,
-        lastEvaluatedKey: { clientId: clients[0].clientId }
-      })
-    }
-
-    expect(result).toEqual(expectedResult)
-  })
-
-  it('should return error for invalid lastEvaluatedKey', async () => {
-    const event = {
-      httpMethod: 'GET',
-      queryStringParameters: {
-        lastEvaluatedKey: 'invalid'
-      },
-      requestContext: {
-        authorizer: {
-          claims: {
-            sub: 'userId'
-          }
-        }
-      }
-    } as unknown as APIGatewayProxyEvent
-
-    const result = await getClientsController(event)
-
-    expect(result.statusCode).toEqual(500)
-    expect(result.body).toStrictEqual(
-      expect.stringContaining('Invalid JSON format for lastEvaluatedKey')
-    )
+    expect(result.statusCode).toBe(500)
   })
 })
