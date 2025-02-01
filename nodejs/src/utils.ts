ea5438ba7b904acfbb23ee675b817902b974d39c
yakhousam <ykhoudir@gmail.com>
2025-02-01 18:46:37 +0100

    Refactor createUpdateExpression to simplify update syntax and remove unused function

--- nodejs/src/utils.ts
+++ nodejs/src/utils.ts
@@ -1,22 +1,7 @@
 export function createUpdateExpression(
   updates: Record<string, string>,
 ): string {
-  return (
-    "SET " +
-    Object.keys(updates)
-      .map((key) => `${key} = :${key}`)
-      .join(", ")
-  );
-}
-
-export function createExpressionAttributeValues(
-  updates: Record<string, string>,
-) {
-  return Object.entries(updates).reduce<Record<string, string>>(
-    (acc, [key, value]) => {
-      acc[`:${key}`] = value;
-      return acc;
-    },
-    {},
-  );
+  return Object.entries(updates)
+    .map(([key, value]) => `"${key}" = '${value}'`)
+    .join(", ");
 }
