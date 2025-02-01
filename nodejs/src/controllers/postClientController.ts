0c1092313bec109a704534744bda6059c5f10990
yakhousam <ykhoudir@gmail.com>
2025-02-01 18:47:09 +0100

    Refactor postClient to use PostgreSQL and handle duplicate client errors

--- nodejs/src/controllers/postClientController.ts
+++ nodejs/src/controllers/postClientController.ts
@@ -1,6 +1,6 @@
-import { ddbDocClient, tableName } from "@/db/client";
-import { Client, createClientSchema } from "@/validation";
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { getClient } from "@/db/client";
+import { DatabaseError } from "pg";
+import { type Client, createClientSchema } from "@/validation";
 import {
   type APIGatewayProxyEvent,
   type APIGatewayProxyResult,
@@ -24,20 +24,35 @@ const postClientController = async (
       updatedAt: new Date().toISOString(),
     };
 
-    const command = new PutCommand({
-      TableName: tableName,
-      Item: newClient,
-    });
+    const dbClient = await getClient();
 
-    await ddbDocClient.send(command);
+    const result = await dbClient.query(
+      `INSERT INTO invoicing_app.clients ("clientId", "userId", "clientName", "email", "phone", "address", "VATNumber", "currencyPreference", "createdAt", "updatedAt") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`,
+      [
+        newClient.clientId,
+        newClient.userId,
+        newClient.clientName,
+        newClient.email,
+        newClient.phone,
+        newClient.address,
+        newClient.VATNumber,
+        newClient.currencyPreference,
+        newClient.createdAt,
+        newClient.updatedAt,
+      ],
+    );
 
     return {
       statusCode: 201,
-      body: JSON.stringify(newClient),
+      body: JSON.stringify(result.rows[0]),
     };
   } catch (error) {
+    console.error(error);
     if (error instanceof ZodError) {
       throw createError.BadRequest(error.message);
+    }
+    if (error instanceof DatabaseError && error.code === "23505") {
+      throw createError.BadRequest(error.message || "Client already exists");
     }
     throw error;
   }
