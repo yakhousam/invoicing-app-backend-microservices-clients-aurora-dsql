e9dec6070b9af9054f9e38f98c8dbe100db089a5
yakhousam <ykhoudir@gmail.com>
2025-02-02 15:17:29 +0100

    Add utility function to retrieve user ID from API Gateway event

--- nodejs/src/utils.ts
+++ nodejs/src/utils.ts
@@ -1,7 +1,17 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+
 export function createUpdateExpression(
   updates: Record<string, string>,
 ): string {
   return Object.entries(updates)
     .map(([key, value]) => `"${key}" = '${value}'`)
     .join(", ");
+}
+
+const isDev = process.env.NODE_ENV === "development";
+
+export function getUserId(event: APIGatewayProxyEvent): string {
+  return isDev
+    ? process.env.userId
+    : event.requestContext.authorizer?.jwt?.claims?.sub;
 }
